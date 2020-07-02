8d7ee93ab9ae06ec066af4ab21d5c39066b5ed1e
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-02 17:03:09 +1200

    get ApplicationDbContext using dependency injection

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -11,6 +11,12 @@ namespace BookListRazor.Pages.BookList
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+
+        //dependency injection
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
 

4b66c3c8ba79b80385d2bdf5bdebc4e7efdf41fb
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-02 16:58:47 +1200

    add application dbContext to Index.cshtml.cs  in order to be able to retrieve data from table

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,7 @@ namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
         public void OnGet()
         {
 

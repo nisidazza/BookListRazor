daca2a03cf33491de5c019291210796518953009
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-03 16:46:11 +1200

    add ApplicationDbContext in BookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookListRazor.Controllers
 {
     public class BookController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();

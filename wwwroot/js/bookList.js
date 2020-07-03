1beff893192572677a0be74aec7a2b2587d46028
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-03 17:48:49 +1200

    fix columns and buttons width

--- wwwroot/js/bookList.js
+++ wwwroot/js/bookList.js
@@ -12,22 +12,22 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            { "data": "name", "width": "30%" },
-            { "data": "author", "width": "30%" },
-            { "data": "isbn", "width": "30%" },
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                             Edit
                         </a>
                     &nbsp;
-                        <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'>
                             Delete
                         </a>
                     </div>`;
-                }, "width": "30%"
+                }, "width": "40%"
             }
         ],
         "language": {

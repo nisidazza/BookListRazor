ca4e48f73a4bb7f8116ef5b9f12b316f7068f88e
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-06 16:16:43 +1200

    implement delete api

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Controllers
 {
@@ -20,9 +21,22 @@ namespace BookListRazor.Controllers
         //implements HTTP GET
         //this API retrieves the books and passes them back when it's called
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data=_db.Book.ToList()});
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, messagge = "Delete successfull" });
         }
     }
 }

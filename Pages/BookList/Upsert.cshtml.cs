1ebab08d46928fcf3989bfc449ddeff4dd63131d
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-06 17:02:34 +1200

    create Upsert page as an example to combine create and update functionalities in the same page...
    ... fix onGet and onPost methods accordingly

---
+++ Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            //in case of create
+            if (id == null)
+            {
+                return Page();
+            }
+
+            //in case of update
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    //this updates every property of the Book
+                    _db.Book.Update(Book);
+                }
+
+                //updates the book object inside the database
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
